f7e9a0732c4bc2cfbb063b5b72c6b79908635e8f
Alvin1992 <fanrx_1992@163.com>
2016-09-06 18:17:02 +0800

    add download_movie

---
+++ download_movie.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Alvin on 2016/8/27.
+ */
+
+// ==UserScript==
+// @name         downlaod movie
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  get download link of http://www.dytt8.net/ and call thunder to download
+// @author       Alvin
+// @include      http://www.dytt8.net/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+})();
